ceecc5a6dbfd453fc1c7df5e2c65ed44f8de4676
Alexandr Ciornii <alexchorny@gmail.com>
2009-09-06 01:20:01 +0300

    Buggy support of locale removed

--- lib/Text/Reform.pm
+++ lib/Text/Reform.pm
@@ -24,8 +24,8 @@ my $emptyref = '';
 
 sub import
 {
-	use POSIX qw( localeconv );
-	$decimal = localeconv()->{decimal_point} || '.';
+	#$decimal = localeconv()->{decimal_point} || '.';
+	$decimal = '.';
 
 	my $lnumerical = '[>]+(?:'.quotemeta($decimal).'[<]{1,})';
 	my $bnumerical = '[]]+(?:'.quotemeta($decimal).'[[]{1,})';
@@ -80,7 +80,7 @@ sub break_with
 sub break_at {
 	my ($hyphen, $opts_ref) = @_;
 	my $hylen = length($hyphen);
-    my $except = $opts_ref->{except};
+	my $except = $opts_ref->{except};
 	my @ret;
 	sub
 	{
@@ -151,6 +151,17 @@ sub notempty
 	return $ne;
 }
 
+sub strtod1 {
+  my $n=shift;
+  my $real_re='((?:[+-]?)(?:(?=[0123456789]|[.])(?:[0123456789]*)(?:(?:[.])(?:[0123456789]{0,}))?)'.
+  '(?:(?:[eE])(?:(?:[+-]?)(?:[0123456789]+))|))';
+  if ($n=~/^\s*$real_re(.*)$/os) {
+    return ($1,length($2 || ''));
+  } else {
+    return (undef,length($n));
+  }
+}
+
 sub replace($$$$)   # ($fmt, $len, $argref, $config)
 {
 	my $ref = $_[2];
@@ -167,9 +178,9 @@ sub replace($$$$)   # ($fmt, $len, $argref, $config)
 	if ($$ref =~ /\S/ && $fmtnum>2)
 	{
 	NUMERICAL:{
-		use POSIX qw( strtod );
 		my ($ilen,$dlen) = map {length} $_[0] =~ m/([]>]+)\Q$decimal\E([[<]+)/;
-		my ($num,$unconsumed) = strtod($$ref);
+		my ($num,$unconsumed) = strtod1($$ref);
+
 		if ($unconsumed == length $$ref)
 		{
 			$$ref =~ s/\s*\S*//;
@@ -999,7 +1010,7 @@ look-up table:
         \@values, \@squares, \@roots, \@logs, \@inverses;
 
 The multiline format specifier:
-        
+
         "| [[  |  [[[  |  [[[[[[[[[[ | [[[[[[[[[ | [[[[[[[[[ |
         -----------------------------------------------------",
 
@@ -1440,7 +1451,7 @@ numeric values about a fixed decimal place marker. For example:
         EONUMS
 
 would print:
-                   
+
         (    1.0 )
         (    1.0 )
         (    1.00)
